4612352478f93ba5158e8804013a1357143b1739
Artchem <chemerys.artem@gmail.com>
2023-10-21 20:37:26 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import { Container, Text } from './App.styled';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App() {
+  const contactsAll = useSelector(state => state.contacts);
+  // console.log('contactsAll :>> ', contactsAll);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <ContactForm />
+      <h2>Contacts</h2>
+
+      <Text>Total contacts: {contactsAll.items.length}</Text>
+
+      <Filter />
+      <ContactList />
+      <ToastContainer autoClose={3000} position="top-right" />
+    </Container>
   );
-};
+}
